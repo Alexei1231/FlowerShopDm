a7e74304658f760589a361260f2e3dac72caec0e
admin <anal0destroyer69@gmail.com>
2022-11-04 23:45:36 +0100

    gotten it right

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -2,14 +2,16 @@
 using System.Collections.ObjectModel;
 using System.Net;
 using System.Reflection.Metadata;
+using System.Xml.Linq;
 
 internal class Program
 {
-    class Flower//товар магазину
+    class Flower//товар магазину(квітка)
     {
         private String Name;
         private int Price;
-        private string Article;
+        private String Article;
+        private bool Availability = true;
 
         public Flower(string name, int price, string article)
         {
@@ -27,6 +29,30 @@ internal class Program
             this.Article = Console.ReadLine();
         }
 
+        public String getArticle()
+        {
+            return this.Article;
+        }
+
+        public String getName()
+        {
+            return this.Name;
+        }
+
+        public int getPrice()
+        {
+            return this.Price;
+        }
+        public void changeStatusToUnavailable()
+        {
+            this.Availability = false;
+        }
+        public void changeStatusToAvailable()
+        {
+           this.Availability = true;
+        }
+
+
     }
 
 
@@ -35,8 +61,8 @@ internal class Program
     {
         private string Address;
         private string Name;
-        private Collection<Flower>? Flowers;
-        private Collection<Employee>? Employees;
+        private Collection<Flower> Flowers = new Collection<Flower>();
+        private Collection<Employee> Employees = new Collection<Employee>();
 
         public FlowerShop(string address, string name, Collection<Flower> flowers, Collection<Employee> employees)
         {
@@ -48,8 +74,8 @@ internal class Program
 
         public FlowerShop()
         {
-      
-            if(this.Name == null)
+
+            if (this.Name == null)//стрінг не може бути налл, треба доробити цей механізм
             {
                 Console.WriteLine("enter the name of the shop:");
                 this.Name = Console.ReadLine();
@@ -59,18 +85,20 @@ internal class Program
                 Console.WriteLine("enter the address of the shop:");
                 this.Address = Console.ReadLine();
             }
-            
+
 
 
         }
 
-        public void addEmployee(Employee flowerShopEmployee)//додавання працівника до штату через код
+        public void addEmployee(Employee employeeToAdd)//додавання працівника до штату через код
         {
-            this.Employees.Add(flowerShopEmployee);
+            this.Employees.Add(employeeToAdd);
         }
-        
+
         public void addEmployee()//додавання працівника до штату через консоль
         {
+            Employee employeeToAdd = new Employee();//"консольне створення" робітника
+            this.Employees.Add(employeeToAdd);
 
         }
 
@@ -86,8 +114,11 @@ internal class Program
             }
             return null;
         }
-        public void fireEmployeeById(string idForSearching)//звільнити робітника
+        public void fireEmployeeById()//(string idForSearching)//звільнити робітника
         {
+            this.showEmployees();
+            Console.WriteLine("write the ID of the employee you want to fire");
+            string idForSearching = Console.ReadLine();
             foreach (var Emp in Employees)
             {
                 if (idForSearching == Emp.getID())
@@ -98,6 +129,79 @@ internal class Program
             }
             Console.WriteLine("we have not found the worker");
 
+        }
+        public void bringEmployeeBack()
+        {
+            this.showEmployees();
+            Console.WriteLine("write the ID of the employee you want to bring back");
+            string idForSearching = Console.ReadLine();
+            foreach (var Emp in Employees)
+            {
+                if (idForSearching == Emp.getID())
+                {
+                    Emp.changeStatusToWorking();
+                    Console.WriteLine("this employee has been brung back successfully");
+                }
+            }
+            Console.WriteLine("we have not found the worker");
+        }
+        public void showEmployees()
+        {
+            foreach(var Emp in Employees)
+            {
+      
+                Console.WriteLine("id: {0}  name: {1}", Emp.getID, Emp.getName);
+
+            }
+        }
+        public void addFlower(Flower flowerToAdd)
+        {
+            this.Flowers.Add(flowerToAdd);
+        }//додавання квітки до списку "через код"
+        public void addFlower()//додавання квітки до списку через консоль
+        {
+            Flower flowerToAdd = new Flower();
+            Flowers.Add(flowerToAdd);
+        }
+        public void showFlowers()
+        {
+            foreach (var Flo in Flowers)
+            {
+                Flo.getArticle();
+                Flo.getName();
+                Flo.getPrice();
+
+            }
+        }
+        public void setFlowerStatusToUnavailable()
+        {
+            this.showFlowers();
+            Console.WriteLine("write the article of the flower you want to change its status to unavailable");
+            string articleForSearching = Console.ReadLine();
+            foreach (var Flo in Flowers)
+            {
+                if (articleForSearching == Flo.getArticle())
+                {
+                    Flo.changeStatusToUnavailable();
+                    Console.WriteLine("this flower is now unavailable");
+                }
+            }
+            Console.WriteLine("we have not found the flower");
+        }
+        public void setFlowerStatusToAvailable()
+        {
+            this.showFlowers();
+            Console.WriteLine("write the article of the flower you want to change its status to available");
+            string articleForSearching = Console.ReadLine();
+            foreach (var Flo in Flowers)
+            {
+                if (articleForSearching == Flo.getArticle())
+                {
+                    Flo.changeStatusToAvailable();
+                    Console.WriteLine("this flower is now available");
+                }
+            }
+            Console.WriteLine("we have not found the flower");
         }
     }
 
@@ -111,7 +215,7 @@ internal class Program
         private bool isWorking;
 
 
-        Employee(string codeOfEmloyee, string name, int age, FlowerShopEmployeePosition employeePosition)//створення працівника
+        public Employee(string codeOfEmloyee, string name, int age, FlowerShopEmployeePosition employeePosition)//створення працівника
         {
             ID = codeOfEmloyee;
             Name = name;
@@ -119,7 +223,7 @@ internal class Program
             this.employeePosition = employeePosition;
             bool isWorking = true;
         }
-        Employee()//створення працівника через консоль(потрібність в проекті цього метода саме в цьому классі не доведена)
+        public Employee()//створення працівника через консоль(потрібність в проекті цього метода саме в цьому классі не доведена)
         {
             Console.WriteLine("name of the worker:");
             this.Name = Console.ReadLine();
@@ -131,6 +235,10 @@ internal class Program
         public void changeStatusToFired()
         {
             this.isWorking = false;
+        }
+        public void changeStatusToWorking()
+        {
+            this.isWorking = true;
         }
 
         public string getID()
@@ -148,10 +256,71 @@ internal class Program
 
     class Prog//клас програми(щоб мінімізувати код в Main)
     {
-        private Collection<FlowerShop> flowerShops;
+        bool workingStage = true;
+        //private Collection<FlowerShop> flowerShops;
+        FlowerShop userFlowerShop;
         public void Start() //стартова сторінка програми; створення першого магазину
         {
-            FlowerShop startingFlowerShop = new FlowerShop();
+            userFlowerShop = new FlowerShop();
+            Menu();
+
+        }
+        public void Menu()
+        {
+            while (workingStage)
+            {
+                Console.WriteLine(" 1 - to add an employee to your shop\n " +
+                    "2 - to add a new flower \n" +
+                    "3 - to show employees \n" +
+                    "4 - to fire an employee\n" +
+                    "5 - to bring an employee back\n" +
+                    "6 - to show items\n" +
+                    "7 - to change item's status to unavailable\n" +
+                    "8 - to change item's status to available\n" +
+                    "9 - to shut the programm down\n");
+                int userChoose = Convert.ToInt32(Console.ReadLine());
+                switch (userChoose)
+                {
+                    case 1:
+                        userFlowerShop.addEmployee();
+                        this.Menu();
+                        break;
+                    case 2:
+                        userFlowerShop.addFlower();
+                        this.Menu();
+                        break;
+                    case 3:
+                        userFlowerShop.showFlowers();
+                        this.Menu();
+                        break;
+                    case 4:
+                        userFlowerShop.fireEmployeeById();
+                        this.Menu();
+                        break;
+                    case 5: 
+                        userFlowerShop.bringEmployeeBack(); 
+                        this.Menu();
+                        break;
+                    case 6:
+                        userFlowerShop.showFlowers();
+                        this.Menu();
+                        break;
+                    case 7:
+                        userFlowerShop.setFlowerStatusToUnavailable();
+                        this.Menu();
+                        break;
+                    case 8:
+                        userFlowerShop.setFlowerStatusToAvailable();
+                        this.Menu();
+                        break;
+                    case 9:
+                        this.workingStage = false;
+                        break;
+
+
+                }
+
+            }
         }
         public Prog()
         {
@@ -166,7 +335,7 @@ internal class Program
 
 
         Prog currentProg = new Prog();
-       
+
 
     }
 }
